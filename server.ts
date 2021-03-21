b1af27e1ffc9d0af34c95f339936bb885b3753d0
jayteedubya <jayteedubya@gmail.com>
2021-03-21 12:27:46 -0700

    still isolating issues

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(upload.none());
 app.use('/style', express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cors());
+app.use(cors());
 
 app.use(upload.none());
 app.use('/api/comments', apiCommentsRouter);
