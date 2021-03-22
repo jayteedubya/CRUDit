8b60342149c3e1af844e33381a7d6503adf97520
jayteedubya <jayteedubya@gmail.com>
2021-03-22 06:39:26 -0700

    jlknk

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ exports.__esModule = true;
 var express = require("express");
 var bodyParser = require("body-parser");
 var multer = require("multer");
-var cors = require("cors");
-var https = require("https");
-var fs = require("fs");
 var postViewsRouter_1 = require("./routes/postViewsRouter");
 var userViewsRouter_1 = require("./routes/userViewsRouter");
 var submitViewsRouter_1 = require("./routes/submitViewsRouter");
@@ -16,12 +13,13 @@ app.set('view engine', 'ejs');
 app.use(upload.none());
 app.use('/style', express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+//app.use(cors());
 app.use('/', postViewsRouter_1["default"]);
 app.use('/user', userViewsRouter_1["default"]);
 app.use('/submit', submitViewsRouter_1["default"]);
-var server = https.createServer({
+/*const server = https.createServer({
     key: fs.readFileSync('./security/server.key'),
     cert: fs.readFileSync('./security/server.cert')
-}, app);
-server.listen(PORT, function () { return console.log("listening on port: " + PORT); });
+  }, app);
+  server.listen(PORT, () => console.log(`listening on port: ${PORT}`)); */
+app.listen(PORT, function () { return console.log("listening on port " + PORT); });
--- server.ts
+++ server.ts
@@ -17,14 +17,16 @@ app.set('view engine', 'ejs');
 app.use(upload.none());
 app.use('/style', express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+//app.use(cors());
 
 app.use('/', postViewsRouter);
 app.use('/user', userViewsRouter);
 app.use('/submit', submitViewsRouter);
 
-const server = https.createServer({
+/*const server = https.createServer({
     key: fs.readFileSync('./security/server.key'),
     cert: fs.readFileSync('./security/server.cert')
   }, app);
-  server.listen(PORT, () => console.log(`listening on port: ${PORT}`));
+  server.listen(PORT, () => console.log(`listening on port: ${PORT}`)); */
+
+  app.listen(PORT, () => console.log(`listening on port ${PORT}`))
