adb2f3072c89604aabd8bc87f9f38e3a59fd06c5
jayteedubya <jayteedubya@gmail.com>
2021-03-27 19:07:20 -0700

    Update userViewsRouter.ts

--- routes/userViewsRouter.ts
+++ routes/userViewsRouter.ts
@@ -20,7 +20,7 @@ userViewsRouter.post('/auth/sign-up', async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(userData.password, 10);
     try {
         const result = await db.users.createUser(userData.username, userData.emailaddress, hashedPassword);
-        res.redirect(`/${result[0].user_name}`);
+        res.redirect(`/users/${result[0].user_name}`);
     }
     catch (err) {
         console.log(err);
