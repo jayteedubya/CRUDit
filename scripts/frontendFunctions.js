6e7342e377a859ec92466612a1be238727aea79a
jayteedubya <jayteedubya@gmail.com>
2021-08-25 21:46:16 -0700

    Update frontendFunctions.js

--- scripts/frontendFunctions.js
+++ scripts/frontendFunctions.js
@@ -241,17 +241,17 @@ const createUser = async () => {
 */
 const addCommentControls = async () => {
     const response = await fetch('/auth/api/who-am-i').catch(err => console.error(err));
-    const username = response.json().username;
+    const username = response.json()
     console.log(username);
     const comments = document.getElementsByClassName('comment-box');
-    console.log(comments);
     Array.from(comments).forEach(comment => {
+        console.log(comment);
         const id = comment.getElementsByClassName('comment-author')[0].dataset.id
         const controls = document.createElement('div');
         controls.setAttribute('class', 'post-control')
         controls.innerHTML(`<ul>
-            <li><button class="post-control" onclick="deleteComment(${id})"> edit   </button></li>
-            <li><button class="post-control" onclick="editComment(${id})"> delete         </button></li>
+            <li><button class="post-control" onclick="deleteComment(${id})"> edit </button></li>
+            <li><button class="post-control" onclick="editComment(${id})"> delete </button></li>
         </ul>`);
         let commentAuthor = comment.getElementsByClassName('comment-author')[0];
         commentAuthor = commentAuthor.dataset.username;
